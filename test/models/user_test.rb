a40eb6e7320590bc916338dae18d5c4ff15f0da6
Ubuntu <ubuntu@ip-172-31-2-174.us-east-2.compute.internal>
2021-11-21 10:02:34 +0000

    emailにRegexを追加、DBレベルで一意性の確保、emailにindexを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,64 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = User.new(name: "testuser", email: "user@test.com")
+  end
+  
+  test "should be valid" do
+    assert @user.valid?
+  end
+  
+  test "name should be presence" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+  
+  test "email should be presence" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+  
+  test "name should not be long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+  
+  test "email should not be long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+  
+  test "email validation should accept valid address" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+    
+  test "email validation should accept reject invalid" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                         foo@bar_baz.com foo@bar+baz.com USER@foo..COM]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+  
+  test "email address should be uniqueness" do
+    user_dup = @user.dup
+    user_dup.email = @user.email.upcase
+    @user.save
+    assert_not user_dup.valid?
+  end
+  
+  test "email address should be saved as lower-case" do
+    mixed_case_email = "FOO@ExaMpeL.Com"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 end
