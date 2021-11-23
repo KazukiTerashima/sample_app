fa3cd86ea566843be1e4bcb7b5f176a0c1c01cfd
Ubuntu <ubuntu@ip-172-31-2-174.us-east-2.compute.internal>
2021-11-23 01:48:47 +0000

    add signup_correct_test and refactor about flash

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,13 +4,26 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup infomation" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: {user: { name: "",
-                                        email: "user@invalid",
-                                        password: "123",
-                                        password_confirmation: "456"}}
+      post users_path, params: { user: { name: "",
+                                      email: "user@invalid",
+                                      password: "123",
+                                      password_confirmation: "456"}}
     end
     assert_template 'users/new'    
     assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
+  end
+  
+  test "valid signup infomation" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "test user",
+                                      email: "test@user.com",
+                                      password: "password",
+                                      password_confirmation: "password"}}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
   end
 end
